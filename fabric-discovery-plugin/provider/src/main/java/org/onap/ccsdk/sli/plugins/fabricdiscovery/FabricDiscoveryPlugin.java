c81a71cba28d9d9338e332131d909237b4710f3c
Arun S. Yerra <arun.yerra@huawei.com>
2017-09-11 18:57:40 -0700

    Implement framework to process REST notifications
    Global SDNC controller needs information about network Devices
    (mostly PNFs) so as to instantiate/configure service. Domain Controllers
    control/manage the network devices within the fabric. This task
    is to implement RESTCONF notification processing framework, so that
    Global SDNC controller can process notifications from Domain controllers.
    
    Change-Id: Ia5edb5006ac3fd4ec71e55f92a05e79e40ce76d2
    Issue-Id: CCSDK-88
    Signed-off-by: Arun S. Yerra <arun.yerra@huawei.com>

---
+++ fabric-discovery-plugin/provider/src/main/java/org/onap/ccsdk/sli/plugins/fabricdiscovery/FabricDiscoveryPlugin.java
@@ -0,0 +1,136 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * openECOMP : SDN-C
+ * ================================================================================
+ * Copyright (C) 2017 AT&T Intellectual Property. All rights
+ * 			reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.ccsdk.sli.plugins.fabricdiscovery;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.Map;
+import java.util.Properties;
+
+import org.onap.ccsdk.sli.core.sli.SvcLogicJavaPlugin;
+import org.onap.ccsdk.sli.core.sli.SvcLogicContext;
+import org.onap.ccsdk.sli.core.sli.SvcLogicException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class FabricDiscoveryPlugin implements SvcLogicJavaPlugin, IFabricDiscoveryService {
+
+    private ExecutorService service;
+    private Map<String, WebSocketClient> streamMap;
+    private static final Logger LOG = LoggerFactory.getLogger(FabricDiscoveryPlugin.class);
+
+    public FabricDiscoveryPlugin() {
+        service = Executors.newFixedThreadPool(10);
+        streamMap = new ConcurrentHashMap<String, WebSocketClient> ();
+    }
+
+    @Override
+    public void processDcNotificationStream (Map<String, String> paramMap, SvcLogicContext ctx) {
+        try {
+            boolean enable = Boolean.parseBoolean(parseParam(paramMap, "enable", true, null));
+            String stream = parseParam(paramMap, "stream", true, null);
+
+            if (enable) {
+                LOG.info("Start monitoring notification stream: " + stream);
+            } else {
+                LOG.info("Stop monitoring  notification stream: " + stream);
+            }
+
+            service.execute(new Runnable () {
+                public void run () {
+                    try {
+                        URI uri = new URI(stream);
+                        if (enable) {
+                            if (streamMap.get(stream) != null) {
+                                LOG.info("Notification Stream: " + stream + " is already being monitoried");
+                                return;
+                            }
+                            IClientMessageCallback messageCallback = new ClientMessageCallback();
+                            WebSocketClient wcClient = new WebSocketClient(uri, messageCallback);
+                            streamMap.put(stream, wcClient);
+                            wcClient.initialize();
+                            try {
+                                wcClient.connect();
+                            } catch (InterruptedException e) {
+                                LOG.info("Web Socket Client throws Exception: " + e.getMessage());
+                            }
+                        } else {
+                            WebSocketClient wc = streamMap.get(stream);
+                            if (wc != null) {
+                                try {
+                                    wc.close("Closing");
+                                } catch (InterruptedException e) {
+                                    LOG.info("Web Socket Client throws Exception: " + e.getMessage());
+                                }
+                            }
+                        }
+                    } catch (URISyntaxException e) {
+                        LOG.info("Exception converting stream to URI with: " + e.getMessage());
+                    }
+                }
+            });
+        } catch (SvcLogicException e) {
+            LOG.info("Parameter Parsing exisited with exeception: ", e.getMessage());
+        }
+    }
+
+    private String parseParam(Map<String, String> paramMap, String name, boolean required, String def)
+        throws SvcLogicException {
+        String s = paramMap.get(name);
+
+        if (s == null || s.trim().length() == 0) {
+            if (!required)
+                return def;
+            throw new SvcLogicException("Parameter " + name + " is required in PropertiesNode");
+        }
+
+        s = s.trim();
+        String value = "";
+        int i = 0;
+        int i1 = s.indexOf('%');
+        while (i1 >= 0) {
+            int i2 = s.indexOf('%', i1 + 1);
+            if (i2 < 0)
+                throw new SvcLogicException("Cannot parse parameter " + name + ": " + s + ": no matching %");
+
+            String varName = s.substring(i1 + 1, i2);
+            String varValue = System.getenv(varName);
+            if (varValue == null)
+                varValue = "";
+
+            value += s.substring(i, i1);
+            value += varValue;
+
+            i = i2 + 1;
+            i1 = s.indexOf('%', i);
+        }
+        value += s.substring(i);
+
+        LOG.info("Parameter " + name + ": " + value);
+        return value;
+    }
+}
