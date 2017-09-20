ba209b4de55c56e5c427e3389518966f2c896661
Dan Timoney <dtimoney@att.com>
2017-09-19 20:48:48 -0400

    Remove unthrown exception
    Merge build fails because catch statement is trying to catch JSONException
    which is not thrown in body of try
    
    Change-Id: I9e29e1698e04f8135799440ca4859aa418d1c54d
    Issue-ID: CCSDK-99
    Signed-off-by: Dan Timoney <dtimoney@att.com>

--- restapi-call-node/provider/src/main/java/org/onap/ccsdk/sli/plugins/restapicall/RestapiCallNode.java
+++ restapi-call-node/provider/src/main/java/org/onap/ccsdk/sli/plugins/restapicall/RestapiCallNode.java
@@ -8,9 +8,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -156,7 +156,7 @@ public class RestapiCallNode implements SvcLogicJavaPlugin {
                             ctx.setAttribute(pp + entry.getKey(), entry.getValue());
                 }
             }
-        } catch (SvcLogicException | JSONException e) {
+        } catch (SvcLogicException e) {
             boolean shouldRetry = false;
             if (e.getCause().getCause() instanceof SocketException) {
                 shouldRetry = true;
