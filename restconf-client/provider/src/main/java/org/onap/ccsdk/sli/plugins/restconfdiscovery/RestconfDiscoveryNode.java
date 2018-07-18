d7d5ea0fe2e9bf6c5222174df2bdd80192b9d6ee
gaurav <gaurav.agrawal@huawei.com>
2018-07-18 19:39:10 +0530

    API for RestconfClient
    API for RestconfClient including:
    - RestconfApiCallNode
    - RestconfDiscoveryNode
    
    Change-Id: Ieda6251b91b6d399cd0106993c3fdd1a440d5b0a
    Issue-ID: CCSDK-371
    Signed-off-by: Gaurav Agrawal <gaurav.agrawal@huawei.com>

---
+++ restconf-client/provider/src/main/java/org/onap/ccsdk/sli/plugins/restconfdiscovery/RestconfDiscoveryNode.java
@@ -0,0 +1,25 @@
+package org.onap.ccsdk.sli.plugins.restconfdiscovery;
+
+import java.util.Map;
+import org.onap.ccsdk.sli.core.sli.SvcLogicContext;
+
+/**
+ * Created by root1 on 18/7/18.
+ */
+public class RestconfDiscoveryNode implements SvcLogicDiscoveryPlugin {
+
+    @Override
+    public void establishSubscription(Map<String, String> paramMap, SvcLogicContext ctx) {
+
+    }
+
+    @Override
+    public void modifySubscription(Map<String, String> paramMap, SvcLogicContext ctx) {
+
+    }
+
+    @Override
+    public void deleteSubscription(Map<String, String> paramMap, SvcLogicContext ctx) {
+
+    }
+}
