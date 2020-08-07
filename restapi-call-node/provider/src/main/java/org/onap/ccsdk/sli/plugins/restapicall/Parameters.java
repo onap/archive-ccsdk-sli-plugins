a66d5461bdda211c861ae6d29b22e3b430e4ffbb
sb5356 <sb5356@us.att.com>
2020-08-07 14:29:24 -0400

    Restapi-call-node: Fix setting truststore, should not set system properties
    Issue-ID: CCSDK-2637
    Signed-off-by: sb5356 <sb5356@att.com>
    Change-Id: Icbbb7eaa9f904e0d51199efd78e36b1fa1fa702b

--- restapi-call-node/provider/src/main/java/org/onap/ccsdk/sli/plugins/restapicall/Parameters.java
+++ restapi-call-node/provider/src/main/java/org/onap/ccsdk/sli/plugins/restapicall/Parameters.java
@@ -8,9 +8,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -36,11 +36,6 @@ public class Parameters {
     public Set<String> listNameList;
     public boolean skipSending;
     public boolean convertResponse;
-    public String keyStoreFileName;
-    public String keyStorePassword;
-    public String trustStoreFileName;
-    public String trustStorePassword;
-    public boolean ssl;
     public String customHttpHeaders;
     public String partner;
     public Boolean dumpHeaders;
